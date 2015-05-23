399c8262e59122fde4f793aa1488b663ef5fcfd9
mhn <w@w.w>
2015-05-23 01:29:28 +0100

    Add watcher script for automagic downloading

---
+++ watcher.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import glob
+from time import sleep
+import os
+import platform
+import subprocess
+import sys
+sys.path.insert(0, os.getcwd())
+
+from ll_functions import upload
+
+def upload_all_files():
+
+    if "cygwin" in platform.platform().lower():
+        clipboard = "/dev/clipboard"
+        sound = "cat completed.wav > /dev/dsp"
+    else:
+        clipboard = "xclip"
+        sound = "aplay completed.wav"
+
+    while True:
+        for file in glob.glob("./files/*"):
+            sleep(0.1)
+
+            print("File: " + file)
+            upload_link = upload(file)
+
+            if upload_link:
+                print(upload_link)
+                subprocess.Popen(
+                        "echo %s > %s" % (upload_link, clipboard),
+                        shell=True)
+                subprocess.Popen(sound, shell=True)
+                os.remove(file)
+
+        sleep(0.5)
+
+if __name__ == "__main__":
+    upload_all_files()
