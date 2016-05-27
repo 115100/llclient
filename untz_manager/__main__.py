9d9fc87b3037ed417aad2aed7bac69403eac4ea4
115100 <115100@users.noreply.github.com>
2016-05-27 15:30:15 +0100

    Finish argparse

--- untz_manager/__main__.py
+++ untz_manager/__main__.py
@@ -6,12 +6,22 @@ def args():
     """Parse and return arguments."""
     parser = argparse.ArgumentParser(description="Convert FLACs to OGGs and "
                                                  "sort into a sensible folder hierarchy.")
+    parser.add_argument("--names", "-n",
+                        default="%n - %t",
+                        dest="pattern",
+                        help="Produce filenames as this string, with %%g, %%a, %%l, %%n, %%t, %%d "
+                             "replaced by genre, artist, album, track number, title, "
+                             "and date, respectively. Also, %%%% gives a literal %%. "
+                             "Defaults to %%n - %%t.")
     parser.add_argument("--output", "-o",
+                        dest="output_files",
                         help="Folder to store output files to.", required=True)
     parser.add_argument("--passthrough", "-p",
+                        dest="ogg_cli_parameters",
                         help="Additional CLI arguments to pass through to oggenc.",
                         default="")
     parser.add_argument("--quality", "-q",
+                        dest="quality",
                         type=float,
                         default=10,
                         help="Sets encoding quality to n, between -1 (low) and 10 (high). "
