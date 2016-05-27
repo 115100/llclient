c1672ee6a40060bb2989b97dae4662c57d65e106
115100 <115100@users.noreply.github.com>
2016-05-28 00:24:45 +0100

    Working multiprocess spawn for oggenc

--- untz_manager/__main__.py
+++ untz_manager/__main__.py
@@ -1,11 +1,22 @@
 """Main entry point for untz."""
 import argparse
+import logging
+import multiprocessing
+import os
+import shutil
+
+from .encoder import encode_file
+
+LOGGER = logging.getLogger(__name__)
 
 
 def get_args():
     """Parse and return arguments."""
     parser = argparse.ArgumentParser(description='Convert FLACs to OGGs and '
                                                  'sort into a sensible folder hierarchy.')
+    parser.add_argument('-b', '--base',
+                        dest='base_dir',
+                        help='Base directory to store output files to.', required=True)
     parser.add_argument('-n', '--names',
                         default='%n - %t',
                         dest='pattern',
@@ -13,9 +24,6 @@ def get_args():
                              'replaced by genre, artist, album, track number, title, '
                              'and date, respectively. Also, %%%% gives a literal %%. '
                              'Defaults to %%n - %%t.')
-    parser.add_argument('-b', '--base',
-                        dest='base_dir',
-                        help='Base directory to store output files to.', required=True)
     parser.add_argument('-p', '--passthrough',
                         dest='ogg_cli_parameters',
                         help='Additional CLI arguments to pass through to oggenc.',
@@ -26,12 +34,73 @@ def get_args():
                         default=10,
                         help='Sets encoding quality to n, between -1 (low) and 10 (high). '
                              'Fractional quality levels such as 2.5 are permitted.')
-    parser.add_argument(dest='input_files', help='List of folder/file inputs.', nargs='+')
+    parser.add_argument('-t', '--threads',
+                        dest='threads',
+                        type=int,
+                        default=multiprocessing.cpu_count(),
+                        help='Thread pool size to spawn.')
+    parser.add_argument(dest='inputs', help='List of folder/file inputs.', nargs='+')
 
     return parser.parse_args()
+
+
+def _preflight_checks():
+    if not shutil.which('oggenc'):
+        raise FileNotFoundError('Oggenc must be in $PATH and be executable.')
+    if not shutil.which('flac'):
+        raise FileNotFoundError('Flac must be in $PATH and be executable.')
+
+
+def _recursive_file_search(entry):
+    for root, _, files in os.walk(entry):
+        for file_entry in files:
+            yield os.path.join(root, file_entry)
+
+
+def _encode_on_filter(args, queue):
+    while True:
+        file_entry = queue.get()
+
+        # FIFO queue so we can do this
+        if file_entry is None:
+            break
+
+        if file_entry.lower().endswith('.flac'):
+            encode_file(file_entry,
+                        args.base_dir.rstrip('/'),
+                        args.pattern,
+                        args.quality,
+                        args.ogg_cli_parameters)
 
 
 def main():
     """Main logic for untz."""
     args = get_args()
-    print(args)
+    _preflight_checks()
+
+    queue = multiprocessing.Queue()
+    processes = [multiprocessing.Process(target=_encode_on_filter,
+                                         args=(args, queue)) for i in range(args.threads)]
+
+    for process in processes:
+        process.start()
+
+    for i in args.inputs:
+        if os.path.isdir(i):
+            for file_entry in _recursive_file_search(i):
+                LOGGER.debug('Encoding "%s"', file_entry)
+                queue.put_nowait(file_entry)
+        else:
+            LOGGER.debug('Encoding "%s"', i)
+            queue.put(i)
+
+    # Send stop signal
+    LOGGER.debug('Sending poison signal.')
+    for _ in range(args.threads):
+        queue.put(None)
+
+    LOGGER.info('Waiting for processes to die.')
+    for process in processes:
+        process.join()
+
+    LOGGER.warning('Program exiting now.')
---
+++ untz_manager/encoder.py
@@ -0,0 +1,62 @@
+"""Encoding related operations"""
+import logging
+import subprocess
+import sys
+
+import taglib
+
+LOGGER = logging.getLogger(__name__)
+
+
+def _get_vorbis_comments(audio_file, pattern):
+    macros = (('%g', 'GENRE'),
+              ('%n', 'TRACKNUMBER'),
+              ('%t', 'TITLE'),
+              ('%d', 'DATE'))
+
+    params_dict = {'%g': '-G',
+                   '%n': '-N',
+                   '%t': '-t',
+                   '%d': '-d'}
+
+    vorbis_comments = {}
+
+    afp = taglib.File(audio_file) # pylint: disable=E1103
+
+    for macro, tag in macros:
+        if macro in pattern:
+            vorbis_comments[params_dict[macro]] = afp.tags.get(tag)[0] or '(none)'
+    vorbis_comments['-a'] = afp.tags.get('ARTIST')[0] or 'Unknown artist'
+    vorbis_comments['-l'] = afp.tags.get('ALBUM')[0] or 'Unknown album'
+
+    afp.close()
+
+    return vorbis_comments
+
+
+def encode_file(audio_file, output_dir, pattern, quality, passthrough):
+    """Run oggenc and encode file, storing in a logical manner."""
+
+    process_args = ['oggenc',
+                    '-q', str(quality),
+                    '-n', '{output_dir}/%a/%l/{pattern}.ogg'.format(output_dir=output_dir,
+                                                                    pattern=pattern)]
+
+    if passthrough:
+        process_args.append(passthrough)
+
+    vorbis_comments = _get_vorbis_comments(audio_file, pattern)
+
+    for tag, value in vorbis_comments.items():
+        process_args.append(tag)
+        process_args.append(value)
+
+    process_args.append(audio_file)
+
+    LOGGER.debug('Running "%s"', ' '.join(process_args))
+    process = subprocess.Popen(process_args)
+    process.communicate()
+
+    if process.returncode:
+        LOGGER.critical('Non-zero return code. Exiting.')
+        sys.exit(process.returncode)
